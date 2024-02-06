f0bbe04fe1be6e3ecb2f8100a11705093e183749
andre.rettig@magellan-net.de <andre.rettig@magellan-net.de>
2024-02-06 13:47:14 +0100

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,8 @@
 export default function Login() {
   return (
-    <main>
+    <main className="h-screen w-screen flex justify-center">
       <div>
-        <h1>Login</h1>
+        <h1 className="text-6xl">Login</h1>
       </div>
     </main>
   );

6bc5367179c0b9052b248c60d92ec931aa7258d5
andre.rettig@magellan-net.de <andre.rettig@magellan-net.de>
2024-02-06 15:41:10 +0100

    update login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import { useLoginStore } from "../store/login-store";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+    const router = useRouter();
+    const { emailInput, setEmailInput, passwordInput, setPasswordInput } =
+        useLoginStore();
     return (
         <main className="h-screen w-screen flex justify-center items-center bg-slate-400">
             <div className="flex flex-col border-opacity-50">
-                <div className="grid p-5 card bg-base-300 rounded-box place-items-center">
+                <div
+                    className="grid p-5 card bg-base-300 rounded-box place-items-center"
+                    onClick={() => router.push("/booking")}
+                >
                     <button className="btn btn-active btn-neutral">GAST</button>
                 </div>
                 <div className="divider">OR</div>
@@ -14,9 +24,11 @@ export default function Login() {
                             <span className="label-text">E-Mail</span>
                         </div>
                         <input
-                            type="text"
+                            type="email"
                             placeholder="Type here"
                             className="input input-bordered w-full max-w-xs"
+                            value={emailInput}
+                            onChange={(e) => setEmailInput(e.target.value)}
                         />
                     </label>
                     <label className="form-control w-full max-w-xs">
@@ -24,9 +36,11 @@ export default function Login() {
                             <span className="label-text">Password</span>
                         </div>
                         <input
-                            type="text"
+                            type="password"
                             placeholder="Type here"
                             className="input input-bordered w-full max-w-xs"
+                            value={passwordInput}
+                            onChange={(e) => setPasswordInput(e.target.value)}
                         />
                     </label>
                     <button className="btn btn-active mt-3 btn-neutral">
@@ -34,7 +48,7 @@ export default function Login() {
                     </button>
                 </div>
                 <div className="flex items-end justify-center mt-3">
-                    <Link href={"./register"} className="link link-neutral">
+                    <Link href={"/register"} className="link link-neutral">
                         Registrieren
                     </Link>
                 </div>

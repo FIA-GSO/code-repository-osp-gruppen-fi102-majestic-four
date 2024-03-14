4d42c587aa43106bd8be694b1c14822c38cdf7f6
André Rettig <andre.rettig@live.com>
2024-03-14 17:21:14 +0100

    update

--- src/app/store/general-store.ts
+++ src/app/store/general-store.ts
@@ -4,11 +4,17 @@ type NotificationPopup =
     | { notificationType: "success" | "error"; message: string }
     | undefined;
 
+type Notification = {
+    id: number;
+    benutzerId: number;
+    nachricht: string;
+};
+
 interface IGeneralState {
     hasNotifications: boolean;
     setHasNotifications: (hasNotifications: boolean) => void;
-    notifications: Array<Object>;
-    setNotifications: (notification: Array<Object>) => void;
+    notifications: Notification[];
+    setNotifications: (notification: Notification[]) => void;
     lastNotification: NotificationPopup;
     setLastNotification: (lastNotification: NotificationPopup) => void;
 }
